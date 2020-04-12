095b6586e3b65e99ae723e3aca9fd846c9cc540f
Kleis Auke Wolthuizen <github@kleisauke.nl>
2020-04-12 14:25:36 +0200

    Revert "better render init stops a race"
    This reverts commit 8840bc8a as it causes an deadlock within
    WASM when a thread is started during vips_init. This could also
    fix the recent Travis flakiness.

--- libvips/include/vips/internal.h
+++ libvips/include/vips/internal.h
@@ -113,7 +113,6 @@ void vips__threadpool_shutdown( void );
 
 void vips__cache_init( void );
 
-void vips__sink_screen_init( void );
 void vips__print_renders( void );
 
 void vips__type_leak( void );
--- libvips/iofuncs/sinkscreen.c
+++ libvips/iofuncs/sinkscreen.c
@@ -1022,8 +1022,8 @@ render_thread_main( void *data, void *user_data )
 	render_running = FALSE;
 }
 
-void
-vips__sink_screen_init( void )
+static void *
+vips_sink_screen_init( void *data )
 {
 	g_assert( !render_thread ); 
 	g_assert( !render_dirty_lock ); 
@@ -1039,6 +1039,8 @@ vips__sink_screen_init( void )
 
 	render_running = TRUE;
 	vips_semaphore_init( &n_render_dirty_sem, 0, "n_render_dirty" );
+
+	return( NULL );
 }
 
 /**
@@ -1098,7 +1100,11 @@ vips_sink_screen( VipsImage *in, VipsImage *out, VipsImage *mask,
 	int priority,
 	VipsSinkNotify notify_fn, void *a )
 {
+	static GOnce once = G_ONCE_INIT;
+
 	Render *render;
+
+	VIPS_ONCE( &once, vips_sink_screen_init, NULL );
 
 	if( tile_width <= 0 || tile_height <= 0 || 
 		max_tiles < -1 ) {
