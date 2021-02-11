043a042afef1236a408c2bf93bf9dac824980de1
Kleis Auke Wolthuizen <github@kleisauke.nl>
2021-02-11 20:44:57 +0100

    Build OpenSlide as a dynamically loadable module
    When OpenSlide is found, it is built as a dynamically loadable
    module by default (i.e. `--with-openslide=module`), this can be
    overridden on the command line with:
    `--without-openslide` (to completely disable OpenSlide)
    `--with-openslide[=yes]` (to restore the previous behavior)

--- libvips/foreign/foreign.c
+++ libvips/foreign/foreign.c
@@ -2301,7 +2301,7 @@ vips_foreign_operation_init( void )
 	vips_foreign_save_tiff_buffer_get_type(); 
 #endif /*HAVE_TIFF*/
 
-#ifdef HAVE_OPENSLIDE
+#if defined(HAVE_OPENSLIDE) && !defined(OPENSLIDE_MODULE)
 	vips_foreign_load_openslide_file_get_type(); 
 	vips_foreign_load_openslide_source_get_type(); 
 #endif /*HAVE_OPENSLIDE*/
